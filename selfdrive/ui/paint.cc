ebdda11e4a054f68cf52b4357e1cad99ed8131ed
hkowald <59242500+hkowald@users.noreply.github.com>
2020-05-07 22:13:37 -0700

    Update paint.cc

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -703,30 +703,7 @@ static void ui_draw_vision_map(UIState *s) {
   nvgFill(s->vg);
 }
 
- #static void ui_draw_vision_face(UIState *s) {
-#  const UIScene *scene = &s->scene;
- # const int face_size = 98;
-#  const int face_x = (scene->ui_viz_rx + face_size + (bdr_s * 2));
- # const int face_y = (footer_y + ((footer_h - face_size) / 2));
- # const int face_img_size = (face_size * 1.5);
- # const int face_img_x = (face_x - (face_img_size / 2));
- # const int face_img_y = (face_y - (face_size / 4));
-#  float face_img_alpha = scene->monitoring_active ? 1.0f : 0.15f;
-#  float face_bg_alpha = scene->monitoring_active ? 0.3f : 0.1f;
- # NVGcolor face_bg = nvgRGBA(0, 0, 0, (255 * face_bg_alpha));
- # NVGpaint face_img = nvgImagePattern(s->vg, face_img_x, face_img_y,
- #   face_img_size, face_img_size, 0, s->img_face, face_img_alpha);
-
-#  nvgBeginPath(s->vg);
- # nvgCircle(s->vg, face_x, (face_y + (bdr_s * 1.5)), face_size);
- # nvgFillColor(s->vg, face_bg);
- # nvgFill(s->vg);
-
-#  nvgBeginPath(s->vg);
-#  nvgRect(s->vg, face_img_x, face_img_y, face_img_size, face_img_size);
-#  nvgFillPaint(s->vg, face_img);
-#  nvgFill(s->vg);
-#}
+ 
 
 static void ui_draw_vision_header(UIState *s) {
   const UIScene *scene = &s->scene;
@@ -759,7 +736,7 @@ static void ui_draw_vision_footer(UIState *s) {
   nvgBeginPath(s->vg);
   nvgRect(s->vg, ui_viz_rx, footer_y, ui_viz_rw, footer_h);
 
-  ui_draw_vision_face(s);
+  //ui_draw_vision_face(s);
 
 #ifdef SHOW_SPEEDLIMIT
    ui_draw_vision_map(s);
